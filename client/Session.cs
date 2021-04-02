43ac341f14f9380f9e7652ee33ebb659f8fae389
dalong <dalong@dalongdeMacBook-Pro.local>
2021-04-02 11:53:11 +0800

    add session

--- client/Session.cs
+++ client/Session.cs
@@ -1,3 +1,9 @@
+using Thrift;
+using Thrift.Transport;
+using Thrift.Protocol;
+using Thrift.Server;
+
+
 namespace iotdb_client_csharp.client
 {
     public class Session
@@ -5,6 +11,7 @@ namespace iotdb_client_csharp.client
        private string username="root", password="root", zoneId, host;
        private int port, fetch_size=10000;
        private long sessionId, statementId;
+       private bool is_close = true;
 
        public Session(string host, int port){
            // init success code 
@@ -19,8 +26,18 @@ namespace iotdb_client_csharp.client
             this.zoneId = zoneId;
             this.fetch_size = fetch_size;
         }
-        
+        public void open(bool enableRPCCompression){
+            if(!is_close){
+                return ;
+            }
 
+            try{
+
+            }
+            catch{
+                
+            }
+        }
        
     }
 }
