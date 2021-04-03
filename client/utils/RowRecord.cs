5135d88403504b06f6d15673b0acb1819fa1b29f
dalong <dalong@dalongdeMacBook-Pro.local>
2021-04-03 09:58:53 +0800

    add RowRecord

---
+++ client/utils/RowRecord.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Thrift;
+namespace iotdb_client_csharp.client.utils
+{
+    public class RowRecord
+    {
+        private long timestamp{get;set;}
+        private List<Field> field_lst{get;set;}
+        public RowRecord(long timestamp, List<Field> field_lst){
+            this.timestamp = timestamp;
+            this.field_lst = field_lst;
+        }
+        public void add_filed(Field field){
+            field_lst.Add(field);
+        }
+
+        public void set_filed(int index, Field filed){
+            field_lst[index] = filed;
+        }
+
+        public void add_filed<T>(TSDataType data_type, T value){
+            var filed = new Field(data_type);
+            filed.set_value(data_type, value);
+            field_lst.Add(filed);
+        }
+        public override string ToString()
+        {
+            var str = timestamp.ToString();
+            foreach(var field in field_lst){
+                str += "\t\t";
+                str += field.ToString();
+            }
+            return str;
+        }
+        
+
+
+
+    }
+}
