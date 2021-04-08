2ff23426aa3dabe8d783254ede29d998ee3ef70e
eedalong <ee_dalong@163.com>
2021-04-08 21:24:27 +0800

    fix bytebuffer bug

--- client/SessionTest.cs
+++ client/SessionTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using iotdb_client_csharp.client.utils;
+
 namespace iotdb_client_csharp.client
 {
     public class SessionTest
@@ -7,13 +10,15 @@ namespace iotdb_client_csharp.client
             var session = new Session("localhost", 6667);
             session.open(false);
             System.Diagnostics.Debug.Assert(session.is_open());
-            System.Console.WriteLine("Test Passed!");
+            System.Console.WriteLine("TestOpen Passed!");
         }
         public void TestOpenFailuer(){
             // by Luzhan
         }
         public void TestClose(){
+            
             // by Luzhan
+
         }
         public void TestCreateStorageGroup(){
             // by Luzhan
@@ -33,11 +38,55 @@ namespace iotdb_client_csharp.client
         public void TestDeleteStorageGroups(){
             // by Luzhan
         }
-        
+        public void TestGetTimeZone(){
+           var session = new Session("localhost", 6667);
+           session.open(false);
+           System.Diagnostics.Debug.Assert(session.is_open());
+           var time_zone = session.get_time_zone(); 
+           Console.WriteLine("TestGetTimeZone Passed!");
+        }
+        public void TestInsertStrRecord(){
+           var session = new Session("localhost", 6667);
+           int status = 0;
+           session.open(false);
+           System.Diagnostics.Debug.Assert(session.is_open());
+           session.delete_storage_group("root.test_group");
+           status = session.set_storage_group("root.test_group");
+           System.Diagnostics.Debug.Assert(status == 0);
+           session.delete_time_series("root.test_group.test_series");
+           status = session.create_time_series("root.test_group.test_series", TSDataType.TEXT, TSEncoding.PLAIN, Compressor.UNCOMPRESSED);
+           System.Diagnostics.Debug.Assert(status == 0);
+           var measures = new List<string>{"s_01", "s_02"};
+           var values = new List<string>{"test_record", "test_record"};
+           status = session.insert_record("root.test_group.test_device", measures, values, 1);
+           System.Diagnostics.Debug.Assert(status == 0);
+           Console.WriteLine("TestInsertStrRecord Passed!");
+        }
+        public void TestInsertRecord(){
+           var session = new Session("localhost", 6667);
+           int status = 0;
+           session.open(false);
+           System.Diagnostics.Debug.Assert(session.is_open());
+           session.delete_storage_group("root.test_group");
+           status = session.set_storage_group("root.test_group");
+           System.Diagnostics.Debug.Assert(status == 0);
+           session.delete_time_series("root.test_group.test_series");
+           status = session.create_time_series("root.test_group.test_series", TSDataType.TEXT, TSEncoding.PLAIN, Compressor.UNCOMPRESSED);
+           System.Diagnostics.Debug.Assert(status == 0);
+           var measures = new List<string>{"s_01"};
+           var values = new List<string>{0.ToString()};
+           var types = new List<TSDataType>{TSDataType.INT32};
+           status = session.insert_record("root.test_group.test_device", measures, values, types, 1);
+           System.Diagnostics.Debug.Assert(status == 0);
+           Console.WriteLine("TestInsertRecord Passed!");
+        }
+
         static void Main(){
             var session_test = new SessionTest();
             session_test.TestOpen();
-            session_test.TestOpenFailuer();
+            session_test.TestGetTimeZone();
+            //session_test.TestInsertStrRecord();
+            session_test.TestInsertRecord();
         }
     }
 
