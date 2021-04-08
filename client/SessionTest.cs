407ed87d0a3453c12ae774296330c167bf25d50b
dalong <dalong@dalongdeMacBook-Pro.local>
2021-04-08 19:27:47 +0800

    add some test interface

--- client/SessionTest.cs
+++ client/SessionTest.cs
@@ -9,10 +9,35 @@ namespace iotdb_client_csharp.client
             System.Diagnostics.Debug.Assert(session.is_open());
             System.Console.WriteLine("Test Passed!");
         }
+        public void TestOpenFailuer(){
+            // by Luzhan
+        }
+        public void TestClose(){
+            // by Luzhan
+        }
+        public void TestCreateStorageGroup(){
+            // by Luzhan
+        }
+        public void TestDeleteStorageGroup(){
+            // by Luzhan
+        }
+        public void TestCreateTimeSeries(){
+            // by Luzhan
+        }
+        public void TestCreateMultiTimeSeries(){
+            // by Luzhan
+        }
+        public void TestDeleteTimeSeries(){
+            // by Luzhan
+        }
+        public void TestDeleteStorageGroups(){
+            // by Luzhan
+        }
         
         static void Main(){
             var session_test = new SessionTest();
             session_test.TestOpen();
+            session_test.TestOpenFailuer();
         }
     }
 
